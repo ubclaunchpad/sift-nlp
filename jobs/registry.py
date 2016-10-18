dd10c6cae1b75c3adc325b88dc49b729db202390
Jordan Schalm <jordan.schalm@gmail.com>
2016-10-17 22:28:42 -0700

    add docs to registry and sample, add symlink at root

--- jobs/registry.py
+++ jobs/registry.py
@@ -1,11 +1,23 @@
 """
-This class registers NLP jobs by name.
+This file contains a mapping from NLP job names to functions.
 """
 
 registry = {}
 
+"""
+Adds a job to the registry
+name <String>
+fn <Function>
+"""
 def register(name, fn):
     registry[name] = fn
 
-def run(name, body):
-    return registry[name](body)
+
+"""
+Runs a job
+name <String>
+payload <Dict>
+"""
+def run(name, payload):
+    if name in registry:
+        return registry[name](payload)
--- jobs/sample.py
+++ jobs/sample.py
@@ -6,11 +6,11 @@ The required components of a Sift NLP job are:
 (2) An explanatation of the JSON-serializable output format of the function
 """
 
-from jobs.registry import register
+from sift.jobs.registry import register
 
 # This sample processing job function returns a list of the number of words
 # in each individual piece of feedback, of the form: [Number]
-def sample(product):
+def sample_job(product):
     return map(lambda f: len(f['body'].split()), product['feedback'])
 
-register('sample', sample);
+register('sample', sample_job);
