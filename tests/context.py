f8ee6564368a2f98679a375835717d2fd3f41be9
Matthew <siunami.matt@gmail.com>
2016-11-05 15:07:18 -0700

    merge conflict fix

--- tests/context.py
+++ tests/context.py
@@ -2,9 +2,7 @@ import os
 import sys
 # Create context for test modules
 sys.path.insert(0, os.path.abspath('.'))
-<<<<<<< Updated upstream
-from siftnlp import core, nlp, parse, sentiment
-=======
+
 from siftnlp import core, nlp, parse
 from jobrunner.jobs import sentiment
->>>>>>> Stashed changes
+
